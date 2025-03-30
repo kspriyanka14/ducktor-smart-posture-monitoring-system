6746ad11807320b93816bb1a19b6c703fcb1a2f1
shreyasbhakta <shreyasbhakta@gmail.com>
2025-03-30 02:05:27 -0400

    UI Integration: needs modification

--- preload.js
+++ preload.js
@@ -1,22 +1,26 @@
-const { contextBridge, ipcRenderer } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron');
 
 // Expose versions info to renderer
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron
-})
+});
+
+contextBridge.exposeInMainWorld('api', {
+  openCamPage: () => {
+    ipcRenderer.send('open-cam-page');
+  }
+});
 
 // Expose notification API to renderer with proper error handling
 contextBridge.exposeInMainWorld('notifications', {
   send: () => {
     try {
-      console.log('Sending notification from preload')
-      ipcRenderer.send('send-notification')
-      return true
+      ipcRenderer.send('send-notification');
+      return true;
     } catch (error) {
-      console.error('Error in preload when sending notification:', error)
-      return false
+      return false;
     }
   }
-})
+});
